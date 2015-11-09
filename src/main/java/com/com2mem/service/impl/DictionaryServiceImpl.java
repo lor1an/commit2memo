fb58036c4044f4575b20d8ebc59fcb5aa9b1ce0a
lor1an <anatoliy.eyespots@gmail.com>
2015-11-09 13:49:58 +0200

    fixed bug with unused tags

--- src/main/java/com/com2mem/service/impl/DictionaryServiceImpl.java
+++ src/main/java/com/com2mem/service/impl/DictionaryServiceImpl.java
@@ -3,7 +3,6 @@ package com.com2mem.service.impl;
 import java.io.IOException;
 import java.net.URL;
 
-import org.apache.commons.io.Charsets;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.oxm.XmlMappingException;
@@ -24,15 +23,17 @@ public class DictionaryServiceImpl implements DictionaryService {
     @Value("${thesaurus}")
     private String thesaurusKey;
 
+    private final String replacementTags = "it|d_link|i_link|dx_ety|dx_def";
+
     private static final String API_URL = "http://www.dictionaryapi.com/api/v1/references/collegiate/xml/%s?key=%s";
 
     public EntryList getEntryListFromDictionary(String word)
             throws XmlMappingException, IOException {
         URL url = new URL(String.format(API_URL, word, dictionaryKey));
+
         EntryList entryList = (EntryList) xstream
-                .unmarshalInputStream(InputStreamFilter.filter(
-                        url.openStream(), Charsets.UTF_8, "<it>([^<]*)</it>",
-                        "$1"));
+                .unmarshalInputStream(InputStreamFilter.filterTags(
+                        url.openStream(), replacementTags));
         return entryList;
     }
 }
--- src/main/java/com/com2mem/util/InputStreamFilter.java
+++ src/main/java/com/com2mem/util/InputStreamFilter.java
@@ -4,14 +4,25 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
 
+import org.apache.commons.io.Charsets;
 import org.apache.commons.io.IOUtils;
-
-import static org.apache.commons.io.IOUtils.toInputStream;
 
 public class InputStreamFilter {
 
-    public static InputStream filter(InputStream stream, Charset charset,
-            String regex, String replacement) throws IOException {
-        return toInputStream(IOUtils.toString(stream, charset).replaceAll(regex, replacement),charset);
+    private static final String TAG_REGEX = "<(%s)>([^<]*)</(%s)>";
+    private static final String REPLACEMENT_GROUP = "$2";
+    private static final Charset DEFAULT_CHARSET = Charsets.UTF_8;
+
+    public static InputStream filterTags(InputStream in, Charset charset,
+            String replacementTags) throws IOException {
+        String stream = IOUtils.toString(in, charset);
+        return IOUtils.toInputStream(stream.replaceAll(
+                String.format(TAG_REGEX, replacementTags, replacementTags),
+                REPLACEMENT_GROUP), charset);
+    }
+
+    public static InputStream filterTags(InputStream in, String replacementTags)
+            throws IOException {
+        return filterTags(in, DEFAULT_CHARSET, replacementTags);
     }
 }
