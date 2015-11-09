c4eecb82e6be60000dc8a8702ae39c5708809915
lor1an <anatoliy.eyespots@gmail.com>
2015-11-09 16:35:31 +0200

    removed comments from context; added handling html files by JSPservlet;

--- src/main/java/com/com2mem/controller/WelcomeController.java
+++ src/main/java/com/com2mem/controller/WelcomeController.java
@@ -4,14 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class WelcomeController {
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-    public ModelAndView welcome(ModelMap model) {
-        return new ModelAndView("index");
+    public String welcome(ModelMap model) {
+        return "index";
     }
 
 }
